73f3cc746d5255e6161c816d091b367373774647
Chintu Jha <chintu.jha@vbsb.in>
2025-07-25 13:54:16 +0530

    changes in api

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL + "/api",
+  baseURL: import.meta.env.VITE_BASE_URL,
 });
   
 export default instance;
